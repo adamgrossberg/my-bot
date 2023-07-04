f0117ba107a971c6836c1b5d12ab583b5d6186fa
adamgrossberg <adamgrossberg@gatech.edu>
2023-07-04 12:07:42 +0200

    added insert player methods to PlayerDatabase

--- PlayerDatabase.py
+++ PlayerDatabase.py
@@ -1,16 +1,26 @@
 
 from pymongo.mongo_client import MongoClient
-
-import Player
+from collections.abc import Iterable
 
 class PlayerDatabase:
     def __init__(self, uri):
         self.client = MongoClient(uri)
         self.db = self.client.db
         self.players = self.db.players
+        self.teams = self.db.teams
     
-    def insert_player(self, player: Player):
-        self.players.insert_one(player.get_database_document())
+    def insert_one_player(self, id, name, team):
+        self.players.insert_one({
+            '_id': id,
+            'name': name,
+            'points': 0,
+            'minutes': 0,
+            'team': team
+        })
+    
+    def insert_many_players(self, players: Iterable):
+        for player in players:
+            self.insert_one_player(player['id'], player['name'], player['team'])
     
     def print_players(self):
         for player in self.players.find():
